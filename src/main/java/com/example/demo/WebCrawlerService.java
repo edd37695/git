9a4ab648fbea21f55033afab4ad68091fcb11f1e
edd37695 <edd37695@gmail.com>
2023-08-15 22:17:35 +0800

    test

--- src/main/java/com/example/demo/WebCrawlerService.java
+++ src/main/java/com/example/demo/WebCrawlerService.java
@@ -19,7 +19,7 @@ public class WebCrawlerService {
 		String url = baseUrl + "?co_id=" + stockCode;
 //    	String url = "https://mops.twse.com.tw/mops/web/ezsearch?co_id=6669";
 		try {
-			// 设置请求头
+			// 设置请求头ajfdkjdjfs
 			Map<String, String> headers = new HashMap<>();
 			headers.put("Accept",
 					"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
